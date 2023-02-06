6433edb3cbada08dc9a1a7241c7cf55cda4eb536
test <astappleid@itstep.kz>
2023-02-06 15:54:46 +0600

    initial commit

---
+++ weather_app_6.02/weather_app_6_02App.swift
@@ -0,0 +1,17 @@
+//
+//  weather_app_6_02App.swift
+//  weather_app_6.02
+//
+//  Created by swift on 06.02.2023.
+//
+
+import SwiftUI
+
+@main
+struct weather_app_6_02App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
